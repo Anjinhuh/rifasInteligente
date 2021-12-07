3cc20c516d5ce102ca7ab1171b598dd8d8573883
Anjinho <65797644+Anjinhuh@users.noreply.github.com>
2021-12-07 13:08:02 -0300

    Alterando responsividade

--- src/App.tsx
+++ src/App.tsx
@@ -18,12 +18,14 @@ function App() {
               <p>Quem somos?</p>
             </div>
           </header>
-          <div className="announceCreate">
-            <p className="firstText">Crie sua rifa conosco</p>
-            <p className="textDiferent">O jeito mais <p className="inteligentText">inteligente </p> de se criar uma rifa</p>
-          </div>
-          <div className="buttonAnounce">
-            <button onClick={() => {alert("Clicado")}}>Criar Rifa</button>
+          <div className="announceFix">
+            <div className="announceCreate">
+              <p className="firstText">Crie sua rifa conosco</p>
+              <p className="textDiferent">O jeito mais <p className="inteligentText">inteligente </p> de se criar uma rifa</p>
+            </div>
+            <div className="buttonAnounce">
+              <button onClick={() => {alert("Clicado")}}>Criar Rifa</button>
+            </div>
           </div>
        </div>
     </div>
